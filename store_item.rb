cf872b3b1dbca31fcf17c0e4b5b1b745f3c6239d
Sam Wells <sfwells0518@gmail.com>
2023-03-21 15:00:56 -0500

    print footballs sentence

--- store_item.rb
+++ store_item.rb
@@ -6,6 +6,8 @@ p "My store sells #{item1[:color]} #{item1[:type]} for $#{item1[:price]}!"
 
 p "My store sells #{item2[:color]} #{item2[:type]} for $#{item2[:price]}!"
 
+p "My store sells #{item3[:color]} #{item3[:type]} for $#{item3[:price]}!"
+
 # item1 = { "color" => "yellow", "price" => 5 }
 # item2 = { "color" => "orange", "price" => 15 }
 # item3 = { "color" => "brown", "price" => 25 }
