19915ba97fb6ee7440ae085abd5cfb1602f3575d
Sam Wells <sfwells0518@gmail.com>
2023-03-21 14:29:57 -0500

    create hashes for store items

---
+++ store_item.rb
@@ -0,0 +1,13 @@
+item1 = { type: "tennis balls", color: "yellow", price: 5 }
+item2 = { type: "basketballs", color: "orange", price: 15 }
+item3 = { type: "footballs", color: "brown", price: 25 }
+
+p "My store sells #{item1[:color]} #{item1[:type]} for $#{item1[:price]}"
+
+# item1 = { "color" => "yellow", "price" => 5 }
+# item2 = { "color" => "orange", "price" => 15 }
+# item3 = { "color" => "brown", "price" => 25 }
+
+# item1 = { :color => "yellow", :price => 5 }
+# item2 = { :color => "orange", :price => 15 }
+# item3 = { :color => "brown", :price => 25 }
